4e40bc73daa112afb173f735734c4f298ca2fd55
Enrique Gil <mahoul@gmail.com>
2024-09-29 17:42:29 +0200

    First commit

---
+++ config-fedora.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+get_required_packages(){
+	cat <<-EOF
+	bat
+	btop
+	colordiff
+	dconf-editor
+	exa
+	feh
+	fontawesome-fonts
+	git
+	gnome-shell-extension-pop-shell
+	gnome-tweaks
+	htop
+	kitty
+	kitty-doc
+	jetbrains-mono-fonts
+	krb5-workstation
+	mc
+	mozilla-fira-sans-fonts
+	net-tools
+	nmap
+	nss-tools
+	powerline
+	powerline-fonts
+	virt-viewer
+	stow
+	tmux
+	tmux-powerline
+	vim
+	vim-airline
+	vim-powerline
+	virt-viewer
+	xdotool
+	xprop
+	xrandr
+	EOF
+}
+
+missing_pkgs(){
+	for pkg in $(get_required_packages); do
+		sudo rpm -q $pkg &>/dev/null || return 1
+	done
+	return 0
+}
+
+[ -d $HOME/bin ] && export PATH=$HOME/bin:$PATH
+
+# Check if all required packages are installed and 
+# launch installation if any of them is missing.
+#
+if ! missing_pkgs; then
+	get_required_packages | xargs sudo yum install -y
+fi
+
+# Enable flatpaks and install them
+#
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y \
+	com.brave.Browser \
+	com.hamrick.VueScan \
+	io.github.mimbrero.WhatsAppDesktop \
+	org.filezillaproject.Filezilla \
+	org.telegram.desktop \
+	com.spotify.Client \
+	org.remmina.Remmina \
+	us.zoom.Zoom \
+	org.flameshot.Flameshot \
+	com.visualstudio.code \
+	org.videolan.VLC
+	
+
+stow -vvv --adopt -t ~/ term-setup i3
+
+bash gnome-settings-tweaks.sh
---
+++ gnome-settings-tweaks.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+
+dconf load 
+gnome-settings-dconf.dump
+gnome-settings-extensions.list
