ac1500ac78da931fac885eeade6a4a08234c839f
Enrique Gil <mahoul@gmail.com>
2024-09-29 22:11:35 +0200

    Rewrite gnome-settings-tweaks.sh

--- config-fedora.sh
+++ config-fedora.sh
@@ -71,6 +71,6 @@ flatpak install -y \
 	org.videolan.VLC
 	
 
-stow -vvv --adopt -t ~/ bash  gnome-settings  htop  kitty  tmux
+#stow -vvv --adopt -t ~/ bash  gnome-settings  htop  kitty  tmux
 
-bash gnome-settings-tweaks.sh
+#bash gnome-settings-tweaks.sh
--- gnome-settings-tweaks.sh
+++ gnome-settings-tweaks.sh
@@ -1,5 +1,16 @@
 #!/bin/env bash
 
-dconf load 
-gnome-settings-dconf.dump
-gnome-settings-extensions.list
+# Add GNOME extensions
+curl -L --output /tmp/user-theme.zip https://extensions.gnome.org/extension-data/user-themegnome-shell-extensions.gcampax.github.com.v58.shell-extension.zip
+curl -L --output /tmp/custom_hot_corners_extended.zip https://github.com/G-dH/custom-hot-corners-extended/releases/latest/download/custom-hot-corners-extended@G-dH.github.com.zip
+curl -L --output /tmp/dash_to_dock.zip https://github.com/micheleg/dash-to-dock/releases/download/extensions.gnome.org-v96/dash-to-dock@micxgx.gmail.com.zip
+curl -L --output /tmp/bingwallpaper.zip https://codeload.github.com/neffo/bing-wallpaper-gnome-extension/zip/refs/heads/master
+
+# Install it from tmp
+for ext in user-theme.zip custom_hot_corners_extended.zip dash_to_dock.zip bingwallpaper.zip; do
+	gnome-extension install -f /tmp/$ext
+done
+
+#Add GNOME keyboard shortcuts and User Theme Legacy
+cat gnome-settings-dconf.dump | dconf load /
+
